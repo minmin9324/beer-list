bbe92ef97512e8ed03078bd6dc5b53f5c5667d75
minmin9324 <minmin93240@gmail.com>
2021-11-12 14:22:20 +0900

    home page 완성

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Card } from "antd";
+import "antd/dist/antd.css";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const history = useHistory();
 
-export default Home
+  return (
+    <Container>
+      <Box>
+        <BeerCardTitle>About a variety of beers!</BeerCardTitle>
+        <BeerCard
+          hoverable
+          cover={<img alt="beerIconImg" src="./images/Flat-Beer.svg" />}
+          onClick={() => history.push("/beerlist")}
+        >
+          <Card.Meta
+            title="You can see the beer list!"
+            description="Click !!"
+          />
+        </BeerCard>
+      </Box>
+    </Container>
+  );
+};
+
+export default Home;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+`;
+
+const Box = styled.div`
+  margin: auto 0;
+`;
+
+const BeerCardTitle = styled.h1`
+  color: RGB(239, 199, 74);
+`;
+
+const BeerCard = styled(Card)`
+  width: 340px;
+  padding: 20px;
+`;
--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,23 @@
-import React from "react";
+import { React } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
+  Redirect,
 } from "react-router-dom";
+import Beerlist from "./Pages/Beerlist";
 import Home from "./Pages/Home";
+import { useHistory } from "react-router-dom";
 // import ReactGA from "react-ga";
 
 const Routes = () => {
-
   return (
     <Router>
-        <Switch>
-          <Route path="/home" component={Home} />
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={() => <Redirect to="/home" />} />
+        <Route path="/home" component={Home} />
+        <Route path="/beerlist" component={Beerlist} />
+      </Switch>
     </Router>
   );
 };
